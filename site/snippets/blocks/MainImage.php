d4e82cb7b3ceffb03aebfc01b00731440ce1147d
Patricio <baptista_patricio@hotmail.com>
2021-01-21 07:22:13 +0000

    mobile updates

--- site/snippets/blocks/MainImage.php
+++
@@ -1,4 +0,0 @@
-
-<?= $page->myBlocksField()->toBlocks() ?>
-<img src="<? $block->image ?>" alt=""
-  class="logo-image desktop-only black-logo">
--- site/templates/home.php
+++ site/templates/home.php
@@ -5,8 +5,8 @@
   <section id="home" class="cream-bg">
     <article class="main-image">
       <?php if($image = $page->photo()->toFile()): ?>
-  <img id="main-image" src="<?= $image->url() ?>" alt="">
-<?php endif ?>
+        <img id="main-image" src="<?= $image->url() ?>" alt="">
+      <?php endif ?>
     </article>
     <div class="info">
      <span class="opening-hours"> <p><?= $page->hours()->or('tuesday -- saturday: 5pm till late')  ?></p>  </span>
@@ -14,24 +14,21 @@
     </div>
   </section>
   <section id="book" class="maroon-bg">
-      <div class="book-wrapper">
+    <div class="book-wrapper">
       <div id="rd-widget-frame" style="max-width: 600px; margin: auto;"></div>
       <input id="rdwidgeturl" name="rdwidgeturl" value="https://booking.resdiary.com/widget/Standard/Candela/4647?includeJquery=false" type="hidden">
       <div class="booking-text">
         <p><?= $page->booking()->kt()->or('For large bookings please <br> email us at hello@candelabar.co.nz <br> or call us on 921 300 88') ?></p>
       </div>
     </div>
-
   </section>
   <section id="sec-images">
     <article class="sec-images-container">
-
       <?php
       $images =  $page->secondaryImages()->toFiles();
       foreach($images as $image): ?>
       <img id="sec-image" src="<?= $image->url() ?>" alt="">
       <?php endforeach ?>
-
     </article>
   </section>
   <section id="menu" class="cream-bg">
@@ -39,11 +36,11 @@
       <div class="">
        <h2 class= "heading">Menu</h2>
         <?= $page->menu() ?>
+        <div class="dessert">
+         <h2 class="heading">Something sweet</h2>
+         <?= $page->dessert() ?>
        </div>
-       <div class="dessert">
-          <h2 class="heading">Something sweet</h2>
-          <?= $page->dessert() ?>
-        </div>
+       </div>
     </article>
   </section>
   <section id="voucher" class="maroon-bg">
