00b74f4754fbdb7370b4a47d9c29fbcbb48bbeaf
Patricio <baptista_patricio@hotmail.com>
2021-01-21 05:26:57 +0000

    mobile updates

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -48,17 +48,4 @@
           <li class="nav-link"><a href="#contact">contact</a></li>
         </ul>
       </nav>
-      <div class="topnav">
-<!-- Navigation links (hidden by default) -->
-<a href="javascript:void(0);" class="icon" onclick="myFunction()">
-  <i class="fa fa-bars"></i>
-</a>
-<div id="myLinks">
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
-</div>
-<!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
-
-</div>
     </header>
